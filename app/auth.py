7cab2b441982e3fea4922e303ca230765c8fba43
bananinhamj <viliczinskimj@gmail.com>
2025-08-19 20:27:07 -0300

    lógica de criação e verificação de usuários

---
+++ app/auth.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+
+def create_user(db: Session, user: schemas.UserCreate):
+	pass
+
+def authenticate_user(db: Session, username: str, password: str):
+	pass
+    
